b7f0bfc2c2c292b65be3b36b26d08c2d8d0e7f55
turtleslow <turtleslow@users.noreply.github.com>
2016-09-08 12:04:48 -0400

    fixing build errors for julia v0.5

--- deps/build.jl
+++ deps/build.jl
@@ -7,9 +7,9 @@ libmongoc = library_dependency(
     aliases = ["libmongoc", "libmongoc-1.0"]
     )
 
-provides(Sources, {
+provides(Sources, Dict(
     URI("http://github.com/mongodb/mongo-c-driver/releases/download/1.0.0/mongo-c-driver-1.0.0.tar.gz") => libmongoc
-    })
+    ))
 
 provides(
     BuildProcess,
@@ -20,7 +20,7 @@ provides(
             
 @osx_only begin
     using Homebrew
-    provides(Homebrew.HB, {"mongo-c" => libmongoc})
+    provides(Homebrew.HB, Dict("mongo-c" => libmongoc))
 end
 
-@BinDeps.install [:libmongoc => :libmongoc]
+@BinDeps.install Dict(:libmongoc => :libmongoc)
